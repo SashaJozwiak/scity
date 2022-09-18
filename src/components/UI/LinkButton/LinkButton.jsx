c2be1c0f16d46442093d7cee87c51b4df67fc5c4
agtugchik <agtugchik13@gmail.com>
2022-09-18 10:44:09 +0300

    feat: add style for buttons

---
+++ src/components/UI/LinkButton/LinkButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LinkButton = () => {
+  return (
+    <></>
+  )
+};
+
+export default LinkButton;
--- src/components/UI/navbar/navbar.jsx
+++ src/components/UI/navbar/navbar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cl from "./navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__links">
-        <Link to="/corporation">Корпорация</Link>
-        <Link to="/news">Новости</Link>
-        <Link to="/partners">Партнеры</Link>
-        <Link to="/contacts">Контакты</Link>
-      </div>
-    </div>
+    <div className={cl.navbar}>
+      <Link className={cl.navbar__link} to="/corporation"><p className={cl.link__text}>Корпорация</p></Link>
+      <Link className={cl.navbar__link} to="/news"><p className={cl.link__text}>Новости</p></Link>
+      <Link className={cl.navbar__link} to="/partners"><p className={cl.link__text}>Партнеры</p></Link>
+      <Link className={cl.navbar__link} to="/contacts"><p className={cl.link__text}>Контакты</p></Link>
+    </div >
   )
 }
 
