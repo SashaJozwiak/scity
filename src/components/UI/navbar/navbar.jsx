ec6f7610f7854ee59dae6cf58e5a9fada06db8c0
agtugchik <agtugchik13@gmail.com>
2022-09-19 21:27:48 +0300

    feat: add style for active button

--- src/components/UI/navbar/navbar.jsx
+++ src/components/UI/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 //import { useNavigate } from "react-router";
 import logo from "../../../assets/logo/logo2_svg.svg"
 import logo2 from "../../../assets/logo/logo_3.svg"
@@ -9,29 +9,29 @@ import cl from "./navbar.module.scss";
 //const navigator = useNavigate();
 
 const Navbar = () => {
+  const isActivePredicate = (isActive) => {
+    return isActive
+      ? `${cl.navbar__link} ${cl.navbar__link_active}`
+      : cl.navbar__link
+  }
   return (
-
-
     <div className={cl.navbar}>
-              <img
-                className="logo"
-                src={logo}
-                alt={"logo"}
-                onClick={() => navigator("..")}
-              />
-      <Link className={cl.navbar__link} to="/corporation"><p className={cl.link__text}>Корпорация</p></Link>
-      <Link className={cl.navbar__link} to="/projects"><p className={cl.link__text}>Проекты</p></Link>
-      <Link className={cl.navbar__link} to="/partners"><p className={cl.link__text}>Партнеры</p></Link>
-      <Link className={cl.navbar__link} to="/contacts"><p className={cl.link__text}>Контакты</p></Link>
-
       <img
-                className="logo"
-                src={logo2}
-                alt={"logo"}
-                onClick={() => navigator("..")}
-              />
-
-
+        className="logo"
+        src={logo}
+        alt={"logo"}
+        onClick={() => navigator("..")}
+      />
+      <NavLink className={({ isActive }) => isActivePredicate(isActive)} to="/projects"><p className={cl.link__text}>Проекты</p></NavLink>
+      <NavLink className={({ isActive }) => isActivePredicate(isActive)} to="/corporation"><p className={cl.link__text}>Корпорация</p></NavLink>
+      <NavLink className={({ isActive }) => isActivePredicate(isActive)} to="/partners"><p className={cl.link__text}>Партнеры</p></NavLink>
+      <NavLink className={({ isActive }) => isActivePredicate(isActive)} to="/contacts"><p className={cl.link__text}>Контакты</p></NavLink>
+      <img
+        className="logo"
+        src={logo2}
+        alt={"logo"}
+        onClick={() => navigator("..")}
+      />
     </div>
   );
 };
