287e433a57de3831abbfbf1c905a9e89e95b6cd3
SashaJozwiak <wmdz88@gmail.com>
2022-09-20 17:55:19 +0300

    feat: main design patch 0.1

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "./pages_scss/main.scss"
 
 const Main = () => {
   return (
-    <div>
-      Main
-    </div>
+/*     <div className="main">
+        <div className="main__background">
+            <div className="main__background-text">
+              <span>Несущие системы стройсити</span>
+            </div>
+        </div>
+      
+    </div> */
+    <div class="background">
+    <div class="transparent">Несущие системы стройсити</div>
+</div>
   );
 };
 
