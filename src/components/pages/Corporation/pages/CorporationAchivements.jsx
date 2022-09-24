683c74651e0eca89f41a877b3cbf42ad81c1b2b4
SashaJozwiak <wmdz88@gmail.com>
2022-09-24 19:03:19 +0300

    feat: final corporation pages

--- src/components/pages/Corporation/pages/CorporationAchivements.jsx
+++ src/components/pages/Corporation/pages/CorporationAchivements.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import pic1 from "../../../../assets/pics/montazh.gif"
+import pic2 from "../../../../assets/pics/montazh2.gif"
+
+import cl from "../../pages_scss/corporationabout.module.scss";
 
 const CorporationAchivements = () => {
   return (
-    <div>
-      Achivements
-    </div>
+  <div className={cl.corporationabout}>
+    
+
+      <div className={cl.project_one}>
+        <h1 className={cl.title}>полный цикл монтажных работ с предварительной контрольной сборкой конструкций на наших производственных площадях</h1>
+        
+        <img className={cl.imgone} src={pic1} alt="montazh pic one" />
+      </div>
+
+
+      <div className={cl.project_one}>
+        <h1 className={cl.title}>Выполним монтаж собственными силами либо организуем шеф-монтаж на всех этапах работ</h1>
+        
+        <img className={cl.imgone} src={pic2} alt="montazh pic two" />
+      </div>
+      
+
+
+  </div>
   );
 };
 
--- src/components/pages/Corporation/pages/CorporationHistory.jsx
+++ src/components/pages/Corporation/pages/CorporationHistory.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import pic1 from "../../../../assets/pics/production_1.jpg"
+import pic2 from "../../../../assets/pics/production_for_transparent.png"
+import pic3 from "../../../../assets/pics/sertpic.jpg"
+
+import cl from "../../pages_scss/corporationabout.module.scss";
 
 const CorporationHistory = () => {
   return (
-    <div>
-      History
+<div className={cl.corporationabout}>
+    <div className={cl.project_one}>
+        <h1 className={cl.title}>Производственная площадка, г. Новосибирск</h1>
+        
+        <img className={cl.imgone} src={pic1} alt="production in novosivirsk" />
     </div>
+
+    <div className={cl.project_one}>
+        <h1 className={cl.title}>Высокотехнологичные 5-ти осевые обрабатывающие центры</h1>
+        
+        <img className={cl.imgone} src={pic2} alt="production in novosivirsk equipment" />
+    </div>
+
+    <div className={cl.project_one}>
+        <h1 className={cl.title}>Пройдены все необходимые сертификационные испытания как в России, так и в Европе</h1>
+        <ul>
+          <li>Общие расчетные принципы в ЦНИИСК имени В.А. Кучеренко.</li>
+          <li>Сертификат ГОСТ Р ИСО 9001-2015, патент на полезную модель.</li>
+          <li>Сертификация на предел огнестойкости RE15, RE 30 по ГОСТ Р 53308-2009.</li>
+          <li>Сертификационные испытания (НИИСФ РААСН, ift Rosenheim GmbH)</li>
+        </ul>
+        
+        <img className={cl.imgone} src={pic3} alt="production in novosivirsk equipment" />
+    </div>
+
+</div>
   );
 };
 
