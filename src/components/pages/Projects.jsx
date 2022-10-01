e74dabaf3eb826e3948614b9c61f0cb81d93e3ec
agtugchik <agtugchik13@gmail.com>
2022-10-01 15:29:53 +0300

    feat: add base map structure

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import map from "../../assets/pics/map.png"
 import cl from "../pages/pages_scss/projects.module.scss"
+import { YMaps, Map, Placemark } from "react-yandex-maps";
 
 const Projects = () => {
   return (
     <div className={cl.mapwrapper}>
-      Projects
-      <img className={cl.map} src={map} alt="map" />
+      <YMaps>
+        <Map
+          width={960}
+          height={720}
+          defaultState={{ center: [55.755864, 37.617698], zoom: 9 }} >
+          <Placemark
+            geometry={[55.755864, 37.617698]}
+            options={{
+              iconLayout: "default#image",
+              iconImageHref: 'https://img.icons8.com/ios-filled/2x/marker-x.png',
+              iconImageSize: [50, 50],
+            }}
+            properties={{
+              hintContent: 'Это хинт',
+              balloonContent: 'Это балун'
+            }}
+            modules={
+              ['geoObject.addon.balloon', 'geoObject.addon.hint']
+            }
+          />
+        </Map>
+      </YMaps>
     </div>
   );
 };
