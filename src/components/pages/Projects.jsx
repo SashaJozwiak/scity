7ece41509383f9a885f0fe63b467c37e4b9de106
agtugchik <agtugchik13@gmail.com>
2022-10-03 17:52:28 +0300

    feat: migrate yandex map from project to contacts

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,35 +1,8 @@
 import React from "react";
-import cl from "../pages/pages_scss/projects.module.scss";
-import { YMaps, Map, Placemark } from "react-yandex-maps";
-import { renderToString } from 'react-dom/server'
 
 const Projects = () => {
-  const content = renderToString(<div className="balloon-content"></div>);
   return (
-    <div className={cl.mapwrapper}>
-      <YMaps>
-        <Map
-          width={960}
-          height={720}
-          defaultState={{ center: [55.755864, 37.617698], zoom: 9 }} >
-          <Placemark
-            geometry={[55.755864, 37.617698]}
-            options={{
-              iconLayout: "default#image",
-              iconImageHref: 'https://img.icons8.com/ios-filled/2x/marker-x.png',
-              iconImageSize: [50, 50],
-            }}
-            properties={{
-              hintContent: 'Это хинт',
-              balloonContent: content
-            }}
-            modules={
-              ['geoObject.addon.balloon', 'geoObject.addon.hint']
-            }
-          />
-        </Map>
-      </YMaps>
-    </div>
+    <div>Projects</div>
   );
 };
 
