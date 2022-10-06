c37f305e9f85b8813d8f58d165cd0389c3ad60ea
SashaJozwiak <wmdz88@gmail.com>
2022-10-06 18:43:40 +0300

    fix: some design elements

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,33 +1,50 @@
 import React from "react";
-import cl from "../pages/pages_scss/contacts.module.scss";
+
 import { YMaps, Map, Placemark } from "react-yandex-maps";
 import { renderToString } from 'react-dom/server'
 
+import marker from './../../../src/assets/logo/logo.gif'
+
+import cl from "../pages/pages_scss/contacts.module.scss";
+
 const Contacts = () => {
-  const content = renderToString(<div className="balloon-content"></div>);
+  //const content = renderToString(<div className={cl.ballooncontent}><h1>ООО «СТРОЙСИТИ», МОСКВА, ул.МНЕВНИКИ, 3/1, оф. 216</h1></div>);
   return (
     <div>
+      
       <div className={cl.mapwrapper}>
+      <div className={cl.contacts} >
+        <div className={cl.contactsblock}>
+        <h1 className={cl.title}>Контакты</h1>
+        
+        </div>
+        
+      </div>
+      
         <YMaps>
+        
           <Map
+        
             width={'100vw'}
             height={'calc(100vh + 50px)'}
-            defaultState={{ center: [55.755864, 37.617698], zoom: 9 }} >
+            defaultState={{ center: [55.773256, 37.498564], zoom: 13 }} >
+              
             <Placemark
-              geometry={[55.755864, 37.617698]}
+              geometry={[55.773256, 37.498564]}
               options={{
                 iconLayout: "default#image",
-                iconImageHref: 'https://img.icons8.com/ios-filled/2x/marker-x.png',
-                iconImageSize: [50, 50],
+                iconImageHref: marker,
+                iconImageSize: [50, 65],
               }}
               properties={{
-                hintContent: 'Это хинт',
-                balloonContent: content
+                hintContent: 'ООО «СТРОЙСИТИ»',
+                //balloonContent: content,
               }}
               modules={
                 ['geoObject.addon.balloon', 'geoObject.addon.hint']
               }
             />
+            
           </Map>
         </YMaps>
       </div>
