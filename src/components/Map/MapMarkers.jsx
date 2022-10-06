c1dbb7488768699ddd470d237dd28e12516af828
agtugchik <agtugchik13@gmail.com>
2022-10-06 22:06:25 +0300

    feat: add img

--- src/components/Map/MapMarkers.jsx
+++ src/components/Map/MapMarkers.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import ModalWindowContent from "../ModalWindow/ModalWidnowContent.jsx";
+import MoscowImg from '../../assets/project-photo/moscow/project1.jpg';
 
 const MapMarkers = [
   {
     city: 'Москва',
     top: '44.5%',
     left: '12.2%',
-    content: <ModalWindowContent city={'Москва'} 
-    text={'«Стеклянная Кора», Парк Зарядье, г. Москва  Объем работ: 8700 кв.м. Проектирование и изготовление'} 
-    img={'../../assets/project-photo/moscow/project1.jpg'} />
+    content: <ModalWindowContent
+      city={'Москва'}
+      text={'«Стеклянная Кора», Парк Зарядье, г. Москва  Объем работ: 8700 кв.м. Проектирование и изготовление'}
+      img={MoscowImg}
+    />
   },
- /*  {
+  {
     city: 'CПб',
     top: '31.4%',
     left: '12.7%',
-    content: <ModalWindowContent city={'Санкт-Петербург'} text={'Еще один замечательный город'} />
-  }, */
+    content: <ModalWindowContent
+      city={'Санкт-Петербург'}
+      text={'Еще один замечательный город'}
+      img={''}
+    />
+  },
 ];
 
 export default MapMarkers;
--- src/components/ModalWindow/ModalWidnowContent.jsx
+++ src/components/ModalWindow/ModalWidnowContent.jsx
@@ -9,12 +9,12 @@ const ModalWindowContent = ({ city, text, img }) => {
     <div>
       <h1 className='content-title'>{city}</h1>
       <div className="project">
-          <p className='content-text'>{text}</p>
-          <img src={img} className='project-photo' alt="just image" width={'100%'} height={'100%'} />
+        <p className='content-text'>{text}</p>
+        <img src={img} className='project-photo' alt="project image" width={'100%'} height={'100%'} />
       </div>
     </div>
 
-    
+
   );
 };
 
