363a0329a77b128aef92809f54589c337d744834
agtugchik <agtugchik13@gmail.com>
2022-10-06 22:23:11 +0300

    feat: add project component

--- src/components/Map/MapMarkers.jsx
+++ src/components/Map/MapMarkers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ModalWindowContent from "../ModalWindow/ModalWidnowContent.jsx";
+import ModalWindowProject from "../ModalWindow/ModalWindowProject.jsx";
 import MoscowImg from '../../assets/project-photo/moscow/project1.jpg';
 
 const MapMarkers = [
@@ -7,21 +8,27 @@ const MapMarkers = [
     city: 'Москва',
     top: '44.5%',
     left: '12.2%',
-    content: <ModalWindowContent
-      city={'Москва'}
-      text={'«Стеклянная Кора», Парк Зарядье, г. Москва  Объем работ: 8700 кв.м. Проектирование и изготовление'}
-      img={MoscowImg}
-    />
+    content: <ModalWindowContent city={'Москва'}>
+      <ModalWindowProject
+        text={'«Стеклянная Кора», Парк Зарядье, г. Москва  Объем работ: 8700 кв.м. Проектирование и изготовление'}
+        img={MoscowImg}
+      />
+      <ModalWindowProject
+        text={'«Стеклянная Кора», Парк Зарядье, г. Москва  Объем работ: 8700 кв.м. Проектирование и изготовление'}
+        img={MoscowImg}
+      />
+    </ModalWindowContent>
   },
   {
     city: 'CПб',
     top: '31.4%',
     left: '12.7%',
-    content: <ModalWindowContent
-      city={'Санкт-Петербург'}
-      text={'Еще один замечательный город'}
-      img={''}
-    />
+    content: <ModalWindowContent city={'Санкт-Петербург'} >
+      <ModalWindowProject
+        text={'Еще один замечательный город'}
+        img={''}
+      />
+    </ModalWindowContent>
   },
 ];
 
--- src/components/ModalWindow/ModalWidnowContent.jsx
+++ src/components/ModalWindow/ModalWidnowContent.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import imageMoscowOne from '../../assets/project-photo/moscow/project1.jpg';
-
-import cl from './ModalWindowContent.scss';
 import './ModalWindowContent.scss';
 
-const ModalWindowContent = ({ city, text, img }) => {
+const ModalWindowContent = ({ city, children }) => {
   return (
     <div>
       <h1 className='content-title'>{city}</h1>
-      <div className="project">
-        <p className='content-text'>{text}</p>
-        <img src={img} className='project-photo' alt="project image" width={'100%'} height={'100%'} />
-      </div>
+      {children}
     </div>
-
-
   );
 };
 
