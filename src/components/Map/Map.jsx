5a43bcb69b22c44cbc4a2c7075c9ee6e145ccb5c
SashaJozwiak <wmdz88@gmail.com>
2022-10-07 05:21:37 +0300

    feat: map design and some styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import cl from './Map.module.scss';
+
 import map from '../../assets/pics/component-map.png';
 import MapButton from '../UI/MapButton/MapButton.jsx';
+
+import cl from './Map.module.scss';
+//import cl_button from '../UI/MapButton/MapButton.module.scss'
 
 const Map = ({ children }) => {
   return (
     <div className={cl.MapContainer}>
+      
       <img className={cl.MapImage} src={map} />
+      
       {children}
+
+      <div className={cl.legend}>
+        <ul className={cl.legendlist}>
+          <li className={cl.legendlistone}>
+            <button className={cl.legendbutton1}></button>
+            <span className={cl.legendlists}>Реализованнные проекты</span></li>
+          <li className={cl.legendlistone}>
+          <button className={cl.legendbutton2}>&nbsp;	&nbsp;	&nbsp;</button>
+            <span className={cl.legendlists}>Поставщики комплектующих и сырья</span>
+          </li>
+          
+        </ul>
+        </div>
+      
     </div>
   );
 };
