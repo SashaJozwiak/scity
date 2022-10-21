e5d50717aff92375c2b9699218018978fb546b3f
agtugchik <agtugchik13@gmail.com>
2022-10-21 14:56:18 +0300

    feat: add burger

--- src/components/UI/navbar/navbar.jsx
+++ src/components/UI/navbar/navbar.jsx
@@ -10,31 +10,57 @@ const {
   contactsIcon,
 } = icons(cl.icons);
 
-const Navbar = () => {
+const Navbar = ({ burgerActive, setBurgerActive }) => {
   const isActiveCheck = (isActive) => {
     return isActive
       ? `${cl.navbar__link} ${cl.navbar__link_active}`
       : cl.navbar__link
   }
+  const navLinkOnClick = () => {
+    if (burgerActive) setBurgerActive(false);
+  };
 
   return (
-    <nav className={cl.navbar}>
-      <NavLink className={({ isActive }) => isActiveCheck(isActive)} to="/corporation">
+    <nav className={burgerActive ? `${cl.navbar} ${cl.navbar_active}` : `${cl.navbar}`}>
+      <NavLink
+        className={({ isActive }) => isActiveCheck(isActive)}
+        to="/corporation" onClick={navLinkOnClick}
+      >
         <p className={cl.link__text}>
           {corporationIcon}
-          Деятельность</p></NavLink>
-      <NavLink className={({ isActive }) => isActiveCheck(isActive)} to="/projects">
+          Деятельность
+        </p>
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActiveCheck(isActive)}
+        to="/projects" onClick={navLinkOnClick}
+      >
         <p className={cl.link__text}>
           {projectsIcon}
-          Проекты</p></NavLink>
-      <NavLink className={({ isActive }) => isActiveCheck(isActive)} to="/partners">
+          Проекты
+        </p>
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActiveCheck(isActive)}
+        to="/partners" onClick={navLinkOnClick}
+      >
         <p className={cl.link__text}>
           {partnersIcon}
-          Партнеры</p></NavLink>
-      <NavLink className={({ isActive }) => isActiveCheck(isActive)} to="/contacts">
+          Партнеры
+        </p>
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActiveCheck(isActive)}
+        to="/contacts" onClick={navLinkOnClick}
+      >
         <p className={cl.link__text}>
           {contactsIcon}
-          Контакты</p></NavLink>
+          Контакты
+        </p>
+      </NavLink>
     </nav>
   );
 };
