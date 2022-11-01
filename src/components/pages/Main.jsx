09c36bae00c463f86bd9512c5ede7dbda9252eaa
Sasha Jozwiak <wmdz88@gmail.com>
2022-11-02 00:10:04 +0300

    feat: scroll and add components in mainpage

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,35 +1,60 @@
 import React from "react";
 import StroyCityLogo from "../../components/UI/StroyCityLogo/StroyCityLogo.jsx"
 
+import Corporation from "./Corporation/Corporation.jsx";
+/* import CorporationMain from "./Corporation/pages/CorporationMain.jsx";
+import CorporationNavbar from "./Corporation/CorporationNavbar.jsx";
+import CorporationRouter from "./Corporation/CorporationRouter.jsx"; */
+
+import Projects from "./Projects/Projects.jsx";
+import Partners from "./Partners.jsx";
+import Contacts from "./Contacts.jsx";
+
 import "./pages_scss/main.scss"
 import cl from "./main.module.scss";
 
 
 const Main = () => {
   return (
-    <div>
-      
+    <div className="wrapper">
+      <div className="main-wrapper">
 
-    <div className="background">
-        <div className="transparent">стройсити  <br /> <p className="subtitle">несущие системы</p></div>
+        <div className="background">
+            <div className="transparent">стройсити  <br /> <p className="subtitle">несущие системы</p></div>
+        </div>
+
+      <div className="corporation">
+          <div className={cl.corporationabout}>
+              <div className={cl.project_one}>
+                <h1 className={cl.transparent}>
+                  {/* <span className={cl2.transparent2}>СТРОЙСИТИ</span> */}
+                  Реализуем самые сложные проекты  и нестандартные решения
+                </h1>
+                {/* <img className={cl.logo} src={logo} alt="logo_stroycity" /> */}
+                {/* <h3 className={cl.title}>Реализуем самые сложные проекты  и нестандартные решения</h3> */}
+
+              </div>
+          </div>
     </div>
-
-    <div className="corporation">
-
-    
-
-      <div className={cl.corporationabout}>
-        <div className={cl.project_one}>
-          <h1 className={cl.transparent}>
-            {/* <span className={cl2.transparent2}>СТРОЙСИТИ</span> */}
-            Реализуем самые сложные проекты  и нестандартные решения
-          </h1>
-          {/* <img className={cl.logo} src={logo} alt="logo_stroycity" /> */}
-          {/* <h3 className={cl.title}>Реализуем самые сложные проекты  и нестандартные решения</h3> */}
-
+        
         </div>
+
+        <div>
+          <Corporation/>
         </div>
+
+        {/*<div>
+          <Projects/>
+        </div> */}
+
+        <div>
+        <Partners/>
         </div>
+
+        <div>
+        <Contacts/>
+        </div>
+        
         </div> 
   );
 };
