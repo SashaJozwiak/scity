e4a489e474f26b88adc12dc486e25409a14ca032
Sasha Jozwiak <wmdz88@gmail.com>
2022-11-01 05:28:41 +0300

    fix: burger + partner

--- src/components/pages/Partners.jsx
+++ src/components/pages/Partners.jsx
@@ -4,6 +4,7 @@ import Gazprom from '../../assets/partners/gazprom2.jpg'
 import Stis from '../../assets/partners/stis1.jpg'
 import Lukoil from '../../assets/partners/lukoil1.jpg'
 import Nesu from '../../assets/partners/nesu1.jpg'
+import AGC from '../../assets/partners/agc1.jpg'
 
 
 import '../pages/pages_scss/partners.scss'
@@ -54,18 +55,18 @@ const Partners = () => {
     </div>
   </div>
 
-  {/* <div className="card">
+  <div className="card">
     <div className="card__face card__face--front">
-      <img className="logo" src={Gazprom} "https://i.loli.net/2019/10/18/uXF1Kx7lzELB6wf.jpg" />
+      <img className="logo" src={AGC} /* "https://i.loli.net/2019/10/18/uXF1Kx7lzELB6wf.jpg" */ />
     </div>
     <div className="card__face card__face--back">
-      <img src="https://i.loli.net/2019/11/03/RtVq2wxQYySDb8L.jpg" />
+      {/* <img src="https://i.loli.net/2019/11/03/RtVq2wxQYySDb8L.jpg" /> */}
       <div className="backtext">
-        <h1 className="titletext">Газпром</h1>
-        <p>Глобальная энергетическая компания. Основные направления деятельности — геологоразведка, добыча, транспортировка, хранение, переработка и реализация газа, газового конденсата и нефти, реализация газа в качестве моторного топлива, а также производство и сбыт тепло- и электроэнергии.</p>
+        <h1 className="titletext">AGC Glass Russia</h1>
+        <p>Мировой лидер в производстве стекла для архитектурно-строительного и автомобильного применения. AGC Glass Russia входит в состав компании AGC Glass Europe – европейского подразделения AGC Inc. – крупнейшего производителя листового стекла в мире.</p>
       </div>
     </div>
-  </div> */}
+  </div>
 
   {/* <div className="card">
     <div className="card__face card__face--front">
