390db64a02090c0677266b4aa093f89057e09475
agtugchik <agtugchik13@gmail.com>
2022-11-02 22:11:53 +0300

    feat: change main

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import StroyCityLogo from "../../components/UI/StroyCityLogo/StroyCityLogo.jsx"
-
 import Corporation from "./Corporation/Corporation.jsx";
 /* import CorporationMain from "./Corporation/pages/CorporationMain.jsx";
 import CorporationNavbar from "./Corporation/CorporationNavbar.jsx";
 import CorporationRouter from "./Corporation/CorporationRouter.jsx"; */
-
 import Projects from "./Projects/Projects.jsx";
 import Partners from "./Partners.jsx";
 import Contacts from "./Contacts.jsx";
-
 import "./pages_scss/main.scss"
 import cl from "./main.module.scss";
 
@@ -18,44 +15,37 @@ const Main = () => {
   return (
     <div className="wrapper">
       <div className="main-wrapper">
-
         <div className="background">
-            <div className="transparent">стройсити  <br /> <p className="subtitle">несущие системы</p></div>
+          <div className="transparent">стройсити  <br /> <p className="subtitle">несущие системы</p></div>
         </div>
-
-      <div className="corporation">
+        <div className="corporation">
           <div className={cl.corporationabout}>
-              <div className={cl.project_one}>
-                <h1 className={cl.transparent}>
-                  {/* <span className={cl2.transparent2}>СТРОЙСИТИ</span> */}
-                  Реализуем самые сложные проекты  и нестандартные решения
-                </h1>
-                {/* <img className={cl.logo} src={logo} alt="logo_stroycity" /> */}
-                {/* <h3 className={cl.title}>Реализуем самые сложные проекты  и нестандартные решения</h3> */}
-
-              </div>
+            <div className={cl.project_one}>
+              <h1 className={cl.transparent}>
+                Реализуем самые сложные проекты  и нестандартные решения
+              </h1>
+            </div>
           </div>
+        </div>
+      </div>
+
+      {/* <div>
+        <Corporation />
+      </div>
+
+      <div>
+        <Projects />
+      </div>
+
+      <div>
+        <Partners />
+      </div>
+
+      <div>
+        <Contacts />
+      </div> */}
+
     </div>
-        
-        </div>
-
-        <div>
-          <Corporation/>
-        </div>
-
-        {/*<div>
-          <Projects/>
-        </div> */}
-
-        <div>
-        <Partners/>
-        </div>
-
-        <div>
-        <Contacts/>
-        </div>
-        
-        </div> 
   );
 };
 
